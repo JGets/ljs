48394d5d81ee26d11878636195a18acd196fa493
Jack Gettings <JGets@me.com>
2015-11-04 14:40:58 -0500

    updated comments

--- l.js
+++ l.js
@@ -79,11 +79,20 @@
 	}
 
 
+	/*
+		Attempts to load a script from urlArr, stopping on the first to be successfully loaded.
+		Invokes callback upon success, passing the URL of the successfully loaded script. Invokes
+		failCallback if unable to load any of the passed URLs.
+	*/
 	function loadScript(urlArr, callback, failCallback){
-		var s = d.createElement('script');
+		//create a new asycn script element
+		var s = d.createElement('script');	
 		s.async = true;
 
+		//make a failure handler
 		var failed = function(){
+			//if there are other URLs we're supposed to fall-back on, 
+			//attempt to use the next on in the list, recursively
 			if(urlArr.length > 1){
 				urlArr.shift();
 				s.parentNode.removeChild(s);
@@ -92,28 +101,44 @@
 					loadScript(urlArr, callback, failCallback);
 				});
 			}
+			//otherwise, there are no other URLs to try, so invoke the user-passed fail callback
 			else{
 				failCallback();
 			}
 		};
+		//bind our fail handler
 		bind(s, 'error', failed);
 
+		//create a handler for successful load, which just unbinds our loader,
+		//and invokes the success callback, passing the URL that we had success with
 		var loaded = function(){
 			unbind(s, 'load', loaded);
 			callback(urlArr[0]);
 		};
+		//bind our load hanlder
 		bind(s, 'load', loaded);
 
+		//set the new script element's src attribute
 		s.src = urlArr[0];
 
+		//add the new script element to the document, inserting it before the first script already on the page
 		var f = d.scripts[0];
 		f.parentNode.insertBefore(s, f);
 	}
 
+
+	/*
+		Attempts to load a stylesheet from urlArr, stopping on the first one to be successfully loaded.
+		Invokes callback upon success, passing the URL of the successfully loaded stylesheet. Invokes
+		failCallback if unable to load any of the passed URLs.
+	*/
 	function loadStyle(urlArr, callback, failCallback){
+		//create a new link element
 		var s = d.createElement('link');
 
+		//create our failuer handler
 		var failed = function(){
+			//if there are still other URLs to try, move on to the next one, recursively
 			if(urlArr.length > 1){
 				urlArr.shift();
 				s.parentNode.removeChild(s);
@@ -122,27 +147,33 @@
 					loadScript(urlArr, callback, failCallback);
 				});
 			}
+			//otherwise, there aren't any other URLs to try, so just invoke the failure handler
 			else{
 				failCallback();
 			}
 		};
+		//bind our fail handler
 		bind(s, 'error', failed);
 
+		//create a handler for when a style successfully loads
 		var loaded = function(){
-			unbind(s, 'load', loaded);
-			callback(urlArr[0]);
+			unbind(s, 'load', loaded);	//unbind this handler
+			callback(urlArr[0]);		//invoke the success callback, passing the successful URL
 		};
-		bind(s, 'load', loaded);
+		bind(s, 'load', loaded);	//bind the load handler
 
+		//set the URL of the stylesheet/link element
 		s.href = urlArr[0];
 
-
+		//add the link element to the end of the <head> element (should be after all other stylesheets)
 		d.getElementsByTagName('head')[0].appendChild(s);
-
 	}
 
+
+	//export our ljs object to the window
 	w.ljs = {
 		script : function(urls, callback, failCallback){
+			//make sure the passed paremeters are of types we expect
 			if(!isArr(urls)){
 				urls = [urls];
 			}
@@ -152,14 +183,13 @@
 			if(!isFn(failCallback)){
 				failCallback = function(){};
 			}
-
+			//attempt to load the script without blocking
 			nonRenderBlocking(function(){
 				loadScript(urls, callback, failCallback);
 			});
-
-			
 		},
 		style : function(urls, callback, failCallback){
+			//make sure the passed paremeters are of types we expect
 			if(!isArr(urls)){
 				urls = [urls];
 			}
@@ -169,18 +199,12 @@
 			if(!isFn(failCallback)){
 				failCallback = function(){};
 			}
-
+			//attempt to load the stylesheet without blocking
 			nonRenderBlocking(function(){
 				loadStyle(urls, callback, failCallback);
 			});
-
-			
 		},
-		nrb : nonRenderBlocking
+		nrb : nonRenderBlocking	//export our non-render blocking function as well for 3rd-party extension
 	};
-
-	// //export our ljs object
-	// w['ljs']['script'] = ljs.script;
-	// w['ljs']['style'] = ljs.style;
 
 })(document, window);
